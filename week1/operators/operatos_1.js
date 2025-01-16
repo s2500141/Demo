fdd9df6d4256b701d26e55d6807c032120869302
s2500141 <s2500141@BC-C02FV0CTQ6LTs-MacBook-Air.local>
2025-01-16 14:59:28 +0200

    end of class commmit

---
+++ week1/operators/operatos_1.js
@@ -0,0 +1,32 @@
+/*
+1. Create two variables, a and b, and assign them integer values (e.g., 8 and 3).
+2. Perform the following operations using a and b, and store the results in separate variables:
+- Add the values (sum).
+- Subtract b from a (difference).
+- Multiply the values (product).
+- Divide a by b (quotient).
+- Find the remainder when a is divided by b (remainder).
+3. Use console.log() to print the resuls of each operation. For example, print: "The sum of a and b is: 11"
+4. Add comments to explain what each operation does.
+5. Change the values of a and b and observe how the results change.
+6. Try the same operations with one positive and one negative number.
+
+*/
+
+let a = 5;
+let b = 3;
+
+let sum = a + b;
+console.log("Sum of a and b is:", sum);
+
+let subtract = a - b;
+console.log("subtract of a and b is:", subtract);
+
+let multiply = a * b;
+console.log("Multiply of a amd b is:", multiply);
+
+let devide = a / b;
+console.log("Divide of a and b is :", divide);
+
+let remainder = a % b;
+console.log("Remainder of a and b is:", divide);
